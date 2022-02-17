c07f7793c584b8f1862a02973d2364092c7a2f7d
rksgr <r_kjha@yahoo.com>
2022-02-17 12:21:51 +0530

    Refactored: Double Binding and Name Validation and display error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,13 +12,26 @@ export class AppComponent {
   userName: string="";
   nameError: string="";
 
+  //UC 1: Data Binding: Display name
+  //UC 2: Property binding- Display logo
   ngOnInit():void{
     this.title= "Hello from Indians!";
     this.imgUrl="./assets/Road_less_travelled.jpg";
     this.url="https://www.bridgelabz.com";
   }
+  //UC 3: Event Binding: Navigate to Bridgelabz website on clicking logo
   onClick($event: any): void{
     console.log("Logo is clicked",$event);
     window.open(this.url, "_blank");
+  }
+  // UC 5: Validate name field and display error
+  onInput($event: any){
+    console.log("change event occurred",$event.data);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if(nameRegex.test(this.userName)){
+      this.nameError = "";
+      return;
+    }
+    this.nameError= "Name is incorrect";
   }
 }
